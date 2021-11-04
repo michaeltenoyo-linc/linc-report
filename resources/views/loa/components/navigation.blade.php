876e9d2196916c1afeab72aad5d7a68087ef5aa6
michaeltenoyo-linc <michaeltenoyo.lincgroup@gmail.com>
2021-11-04 11:15:40 +0700

    front end

--- resources/views/loa/components/navigation.blade.php
+++ resources/views/loa/components/navigation.blade.php
@@ -9,7 +9,7 @@
         </button>
         <a class="md:block text-left md:pb-2 text-blueGray-600 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0"
             href="#">
-            LAUTAN LUAS CUSTOMER
+            Letter of Agreements
         </a>
         <ul class="md:hidden items-center flex flex-wrap list-none">
             <li class="inline-block relative">
--- resources/views/master/modals/login-modal.blade.php
+++ resources/views/master/modals/login-modal.blade.php
@@ -11,12 +11,12 @@
             <form>
                 <div class="form-group">
                     <label for="exampleInputEmail1">Username</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                    <input name="username" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter username">
                     <small id="emailHelp" class="form-text text-muted">Never share your username with anyone else.</small>
                 </div>
                 <div class="form-group">
                     <label for="exampleInputPassword1">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                    <input name="password" type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
                 </div>
                 <button type="submit" class="btn btn-primary">Login</button>
             </form>
