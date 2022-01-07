84449381f1408a2342dbc2a77d87642fd7c8e527
michaeltenoyo-linc <michaeltenoyo.lincgroup@gmail.com>
2022-01-07 09:34:30 +0700

    refactory seeder company dengan detail area

--- database/seeds/CompanyLocationSeeder.php
+++ database/seeds/CompanyLocationSeeder.php
@@ -10,6 +10,7 @@
 use App\Models\Company;
 use App\Models\dloa_transport;
 use App\Models\Loa_transport;
+use App\Models\postal_code;
 use Illuminate\Database\Seeder;
 use AzisHapidin\IndoRegion\RawDataGetter;
 use Carbon\Carbon;
@@ -38,6 +39,7 @@ class CompanyLocationSeeder extends Seeder
             error_log($counter,0);
             $counter++;
             if (!$firstline){
+                $postalBPS = postal_code::where('postal_code',$data['8'])->first();
                 Company::create([
                     'reference' => $data['0'],
                     'location' => $data['1'],
@@ -45,8 +47,10 @@ class CompanyLocationSeeder extends Seeder
                     'address_2' => $data['3'],
                     'address_3' => $data['4'],
                     'country' => $data['5'],
-                    'city' => $data['6'],
-                    'province' => $data['7'],
+                    'urban' => $postalBPS->urban,
+                    'district' => $postalBPS->district,
+                    'city' => $postalBPS->city,
+                    'province' => $postalBPS->province,
                     'postal_code' => $data['8'],
                     'timezone' => $data['9'],
                     'latitude' => $data['10'],
