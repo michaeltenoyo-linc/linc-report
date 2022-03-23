f351a4a92e1dae26688726d9cfea58b4a2cdb4a6
michaeltenoyo-linc <michaeltenoyo.lincgroup@gmail.com>
2022-03-23 13:10:43 +0700

    search bj data with datalist

--- resources/views/loa/pages/nav-loa-search-billable.blade.php
+++ resources/views/loa/pages/nav-loa-search-billable.blade.php
@@ -26,12 +26,13 @@ Linc | Input LOA
                     <div class="relative w-full mb-3">
                         <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
                             htmlFor="name">Customer</label>
-                        <select required id="customer" name="customer" class="input-route-start border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" type="text">
-                            <option value="-1">==Pilih Salah Satu==</option>
+                        
+                        <input required name="customer" class="input-divisi border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" type="text" list="customers">
+                        <datalist id="customers">
                             @foreach ($customer as $c)
                                 <option value="{{ $c->reference ?? '' }}">{{ $c->name ?? '' }}</option>
                             @endforeach
-                        </select>
+                        </datalist>
                     </div>
                 </div>
 
