a2a83a4170ec88afcebd2b554148c368f955bbcd
michaeltenoyo-linc <michaeltenoyo.lincgroup@gmail.com>
2022-06-13 14:52:23 +0700

    shipment seeder optimization

--- database/seeds/ShipmentBlujaySeeder.php
+++ database/seeds/ShipmentBlujaySeeder.php
@@ -45,26 +45,45 @@ class ShipmentBlujaySeeder extends Seeder
         
         $counter = 1;
         while(($data = fgetcsv($csvFile, 0, ',','"')) != FALSE){
-            error_log($counter,0);
             $counter++;
             if (!$firstline){
-                $exist = ShipmentBlujay::find($data['5']);
+                try {
+                    ShipmentBlujay::create([
+                        'order_number'=> $data['5'],
+                        'customer_reference'=>$data['0'],
+                        'customer_name'=>$data['1'],
+                        'load_id'=>$data['2'],
+                        'load_group'=>$data['3'],
+                        'billable_total_rate'=>round(floatval(str_replace(',','',$data['6'])),2),
+                        'payable_total_rate'=>round(floatval(str_replace(',','',$data['9'])),2),
+                        'load_closed_date' => ($this->checkDateString($data['7']))?Carbon::createFromFormat('d/m/Y H:i', $data['7']):null,
+                        'load_status' => $data['8'],
+                    ]);
 
-                if(!is_null($exist)){
-                    $exist->forceDelete();
+                    error_log("Individual Shipment :".$counter." (New)",0);
+                } catch (\Throwable $th) {
+                    $exist = ShipmentBlujay::find($data['5']);
+
+                    if(!is_null($exist)){
+                        $exist->forceDelete();
+                    }
+
+                    ShipmentBlujay::create([
+                        'order_number'=> $data['5'],
+                        'customer_reference'=>$data['0'],
+                        'customer_name'=>$data['1'],
+                        'load_id'=>$data['2'],
+                        'load_group'=>$data['3'],
+                        'billable_total_rate'=>round(floatval(str_replace(',','',$data['6'])),2),
+                        'payable_total_rate'=>round(floatval(str_replace(',','',$data['9'])),2),
+                        'load_closed_date' => ($this->checkDateString($data['7']))?Carbon::createFromFormat('d/m/Y H:i', $data['7']):null,
+                        'load_status' => $data['8'],
+                    ]);
+
+                    error_log("Individual Shipment :".$counter." (Exist Updating)",0);
                 }
 
-                ShipmentBlujay::create([
-                    'order_number'=> $data['5'],
-                    'customer_reference'=>$data['0'],
-                    'customer_name'=>$data['1'],
-                    'load_id'=>$data['2'],
-                    'load_group'=>$data['3'],
-                    'billable_total_rate'=>round(floatval(str_replace(',','',$data['6'])),2),
-                    'payable_total_rate'=>round(floatval(str_replace(',','',$data['9'])),2),
-                    'load_closed_date' => ($this->checkDateString($data['7']))?Carbon::createFromFormat('d/m/Y H:i', $data['7']):null,
-                    'load_status' => $data['8'],
-                ]);
+                
             }
 
             $firstline = false;
